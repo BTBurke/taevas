efacd2206b727db5faa52dae02ac215ea4c32b68
Bryan Burke <bryan.t.burke@gmail.com>
2022-01-02 18:34:53 +0200

    refactor for main.go location

--- cmd/taevas/main.go
+++ cmd/taevas/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"log"
 	"os"
+	"path/filepath"
 
+	"github.com/BTBurke/taevas/utils"
 	"github.com/magefile/mage/mage"
 )
 
 func main() {
+
+	root := utils.GoRoot()
+
+	if err := os.Chdir(filepath.Join(root, ".taevas")); err != nil {
+		log.Fatal(err)
+	}
+
 	os.Exit(mage.Main())
 }
