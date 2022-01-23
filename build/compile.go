b1269e0a4e7fe58699c198310f3e08cd82fde04c
Bryan Burke <bryan.t.burke@gmail.com>
2022-01-23 14:48:07 +0200

    WIP build interface

--- build/compile.go
+++ build/compile.go
@@ -1,10 +1,14 @@
 package build
 
 import (
-	"context"
+	"fmt"
+	"os"
+	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/BTBurke/taevas/build/fs"
+	"github.com/BTBurke/taevas/utils"
 )
 
 type Context struct {
@@ -14,25 +18,77 @@ type Context struct {
 	options options
 	in      *fs.Filesystem
 	out     *fs.Filesystem
-	ctx     context.Context
+}
+
+func New(root string, opts ...BuildOption) (*Context, error) {
+	// sets up defaults which can be overridden by later options
+	allOpts := append([]BuildOption{
+		withRoot(root),
+		WithOutputDirectory("dist", true),
+		WithTemplateExtension(".tmpl"),
+		WithTimeout(30 * time.Second),
+	}, opts...)
+
+	var o options
+	for _, opt := range allOpts {
+		if err := opt(&o); err != nil {
+			return nil, fmt.Errorf("error setting build option: %w", err)
+		}
+	}
+
 }
 
 type BuildOption func(*options) error
 
 type options struct {
-	templateExt string
-	root        string
-	outDir      string
+	templateExt     string
+	root            string
+	outDir          string
+	outDirOverwrite bool
+	timeout         time.Duration
 }
 
-var DefaultOptions = options{
-	templateExt: ".tmpl",
-	outDir:      "dist",
-}
-
-func WithTemplateExtension(ext string) {
-	if !strings.HasPrefix(ext, ".") {
-		ext = "." + ext
+func WithTemplateExtension(ext string) BuildOption {
+	return func(o *options) error {
+		if !strings.HasPrefix(ext, ".") {
+			ext = "." + ext
+		}
+		o.templateExt = ext
+		return nil
 	}
+}
 
+func withRoot(root string) BuildOption {
+	return func(o *options) error {
+		// root can be relative or absolute directory. If relative, it should refer to Go module root
+		p := utils.ParsePath(root)
+		if !p.IsAbs() {
+			root = filepath.Join(utils.GoRoot(), root)
+		}
+
+		if _, err := os.Stat(root); os.IsNotExist(err) {
+			return fmt.Errorf("error setting build options: root %s does not exist (%w)", root, err)
+		}
+		o.root = root
+		return nil
+	}
+}
+
+func WithOutputDirectory(outputDirectory string, overwrite bool) BuildOption {
+	return func(o *options) error {
+		p := utils.ParsePath(outputDirectory)
+		if !p.IsAbs() {
+			outputDirectory = filepath.Join(utils.GoRoot(), outputDirectory)
+		}
+		o.outDir = outputDirectory
+		o.outDirOverwrite = overwrite
+		return nil
+	}
+}
+
+func WithTimeout(d time.Duration) BuildOption {
+	return func(o *options) error {
+		o.timeout = d
+		return nil
+	}
 }
